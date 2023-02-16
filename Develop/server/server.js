232e3a5e547ad3212431d7c7fbbf434c92b36f1b
ricky-rosay <111669764+ricky-rosay@users.noreply.github.com>
2023-02-16 18:17:25 -0500

    adding new starter code

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -6,13 +6,6 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers, 
-  context: authMiddleware,
-  persistedQueries: false, 
-});
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
